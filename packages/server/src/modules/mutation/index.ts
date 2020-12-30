8e6c89861174075a432cec011688aefe758d9d01
Wellerson <wellerson.moreira@intelie.com.br>
2020-12-30 00:17:03 -0300

    add: update an item  mutation

--- packages/server/src/modules/mutation/index.ts
+++ packages/server/src/modules/mutation/index.ts
@@ -2,10 +2,12 @@ import LoginUserMutation from './user/Login'
 import RegisterUserMutation from './user/Register'
 import CreateItemMutation from './item/Create'
 import DeleteItemMutation from './item/Delete'
+import UpdateItemMutation from './item/Update'
 
 export default {
     LoginUserMutation,
     RegisterUserMutation,
     CreateItemMutation,
     DeleteItemMutation,
+    UpdateItemMutation,
 }
