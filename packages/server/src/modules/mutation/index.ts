6fbac1a70b1f8828994deeb6901a1601017da067
Wellerson <wellerson.moreira@intelie.com.br>
2020-12-29 23:56:22 -0300

    add: DeleteItemMutation on index mutation

--- packages/server/src/modules/mutation/index.ts
+++ packages/server/src/modules/mutation/index.ts
@@ -1,9 +1,11 @@
 import LoginUserMutation from './user/Login'
 import RegisterUserMutation from './user/Register'
 import CreateItemMutation from './item/Create'
+import DeleteItemMutation from './item/Delete'
 
 export default {
     LoginUserMutation,
     RegisterUserMutation,
     CreateItemMutation,
+    DeleteItemMutation,
 }
