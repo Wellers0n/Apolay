c11871fae8a223ee271e0a53a2cff279b32f8a1e
Wellerson <wellerson.moreira@intelie.com.br>
2021-01-22 02:25:37 -0300

    init project web :rocket:

---
+++ packages/web/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (
+      <div>Home page</div>
+  )
+}
+
+export default Home;
---
+++ packages/web/src/router.tsx
@@ -0,0 +1,52 @@
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom'
+import history from 'helpers/history';
+import GlobalStyle from 'styles/global';
+import { SnackbarProvider } from 'notistack';
+// import PrivateRouter from 'components/PrivateRouter'
+// Pages
+const Home = lazy(() => import('pages/Home' /* webpackChunkName: "Home" */))
+
+const router = () => {
+
+    return (
+        <SnackbarProvider
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            maxSnack={3}
+        >
+            <BrowserRouter>
+                <div>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Router history={history}>
+                            <Switch>
+                                <Route exact path="/" component={Home} />
+                                {/* <Route path="/login" component={Login} />
+                                        <Route path="/register" component={Register} />
+                                        <Route path="/forgot" component={Forgot} />
+                                        <PrivateRouter path="/profile" component={Profile} />
+                                        <PrivateRouter path="/companies" component={Companies} />
+                                        <PrivateRouter path="/modules/:id" component={Modules} />
+                                        <PrivateRouter path="/partners" exact component={Partners} />
+                                        <PrivateRouter path="/partners/:id" component={PartnersDetail} /> */}
+                                {/* <PrivateRouter path="/home" component={Home} /> */}
+                                <Route component={notFound} />
+                                <GlobalStyle />
+                            </Switch>
+                        </Router>
+                    </Suspense>
+                </div>
+            </BrowserRouter>
+        </SnackbarProvider>
+    )
+}
+
+const notFound = () => {
+    return (
+        <div>pag not found</div>
+    )
+}
+
+export default router
