dad1e49982ec944bf9aee45dc2700954a11aa510
Wellerson <wellerson.moreira@intelie.com.br>
2021-01-22 01:42:04 -0300

    add SubscriptionServer

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,6 +3,11 @@ import { createServer } from "http";
 import app from "./app";
 import connectDatabase from "./database";
 import { graphqlPort } from './config';
+import { SubscriptionServer } from 'subscriptions-transport-ws'
+import { getUser } from './auth';
+import { getContext } from './getContext'
+import { execute, subscribe } from 'graphql';
+import schema from './schema'
 
 (async () => {
   try {
@@ -19,4 +24,28 @@ import { graphqlPort } from './config';
       `SERVER ON: http://localhost:${graphqlPort}/graphql`
     );
   });
+
+  // server subscription
+  type ConnectionParams = {
+    authorization?: string;
+  };
+  SubscriptionServer.create(
+    {
+      onConnect: async (connectionParams: ConnectionParams) => {
+        const { user } = await getUser(connectionParams.authorization);
+
+        return await getContext({ user });
+      },
+      // eslint-disable-next-line
+      onDisconnect: () => console.log('Client subscription disconnected!'),
+      execute,
+      subscribe,
+      schema,
+    },
+    {
+      server,
+      path: '/subscriptions',
+    },
+  );
+
 })();
