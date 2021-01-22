01db75bf6904cdefb7bfa5bfafc2b0d291d22b8d
Wellerson <wellerson.moreira@intelie.com.br>
2021-01-22 01:42:35 -0300

    add subscription to schema

--- packages/server/src/schema.ts
+++ packages/server/src/schema.ts
@@ -1,10 +1,12 @@
 import { GraphQLSchema } from 'graphql'
 import QueryType from './types/QueryType'
 import MutationType from './types/MutationType'
+import SubscriptionType from './types/SubscriptionType'
 
 const schema =  new GraphQLSchema({
     query: QueryType,
-    mutation: MutationType
+    mutation: MutationType,
+    subscription: SubscriptionType,
 })
 
 export default schema
